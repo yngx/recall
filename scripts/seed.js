f4ac42abd40ac9af87bd7589387d6967bea7d419
Xao <divxyng.edu@gmail.com>
2024-01-03 23:28:06 -0800

    added a seed file and a few dependencies

---
+++ scripts/seed.js
@@ -0,0 +1,65 @@
+const { db } = require('@vercel/postgres')
+const bcrypt = require('bcrypt')
+const dotenv = require('dotenv');
+const path = require('path');
+
+const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}.local` : '.env';
+dotenv.config({ path: path.resolve(process.cwd(), envFile) });
+
+const users = [
+  {
+    id: '1',
+    name: 'John Doe',
+    email: 'example.com',
+    password: '1234',
+  }
+]
+
+async function seedUsers(client) {
+  try {
+    await client.sql`CREATE EXTENSION IF NOT EXISTS "uuid-ossp"`;
+    // Create the "users" table if it doesn't exist
+    const createTable = await client.sql`
+      CREATE TABLE IF NOT EXISTS users (
+        id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
+        name VARCHAR(255) NOT NULL,
+        email TEXT NOT NULL UNIQUE,
+        password TEXT NOT NULL
+      );
+    `;
+
+    console.log(`Created "users" table`);
+
+    // Insert data into the "users" table
+    const insertedUsers = await Promise.all(
+      users.map(async (user) => {
+        const hashedPassword = await bcrypt.hash(user.password, 10);
+        return client.sql`
+        INSERT INTO users (id, name, email, password)
+        VALUES (${user.id}, ${user.name}, ${user.email}, ${hashedPassword})
+        ON CONFLICT (id) DO NOTHING;
+      `;
+      }),
+    );
+
+    console.log(`Seeded ${insertedUsers.length} users`);
+
+    return {
+      createTable,
+      users: insertedUsers,
+    };
+  } catch (error) {
+    console.error('Error seeding users:', error);
+    throw error;
+  }
+}
+
+async function main() {
+  const client = await db.connect();
+
+  await seedUsers(client);
+
+  await client.end();
+}
+
+main().catch((error) => console.error(error));
